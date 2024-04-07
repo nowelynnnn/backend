78b273b4954c59636c82b592cb6fdd4e28d0dd68
Nowelyn <lynnecabutaje@gmail.com>
2024-04-07 22:00:16 +0800

    Created Storey functionality in CarouselItemsControllerl

--- app/Http/Controllers/Api/CarouselItemsController.php
+++ app/Http/Controllers/Api/CarouselItemsController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Models\CarouselItems;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CarouselItemsRequest;
+use Illuminate\Auth\Events\Validated;
 
 class CarouselItemsController extends Controller
 {
@@ -13,17 +15,21 @@ class CarouselItemsController extends Controller
      */
     public function index()
     {
-        return CarouselItems::all();
+        return CarouselItems::all(); 
     }
 
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CarouselItemsRequest $request)
     {
-        //
-    }
+         // Retrieve the validated input data...
+        $validated = $request->validated();
+        $CarouselItem = CarouselItems::create($validated);
+
+        return $CarouselItem;
+    } 
 
     /**
      * Display the specified resource.
